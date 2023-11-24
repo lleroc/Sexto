dbe73c9b84b5b3151efdf9e37924c248cd1ae715
Lleroc <llleroc@yahoo.es>
2023-11-23 20:12:16 -0500

    operaciones_matematricas

--- Proyecto3/views/index.view.php
+++ Proyecto3/views/index.view.php
@@ -1,23 +1,26 @@
 <!DOCTYPE html>
-<html lang="en">
-
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+    <script src="operaciones.js"></script>
+    <style>
+    ul li{
+        list-style: none;
+        float: left;
+        margin: 10px;
+    }
+</style>
 </head>
-
 <body>
-
     <div class="container">
-
         <div class="row">
             <div class="col-12">
                 <h1>Hello World</h1>
             </div>
-
             <div class="col-md-12">
                 <label for="" class="control-label">Suma</label>
                 <div class="form-group">
@@ -49,17 +52,7 @@
                     </li>
                 </ul>
             </div>
-<style>
-    ul li{
-        list-style: none;
-        float: left;
-        margin: 10px;
-
-    }
-</style>
         </div>
     </div>
-
 </body>
-
 </html>
--- Proyecto3/views/operaciones.js
+++ Proyecto3/views/operaciones.js
@@ -1,7 +1,15 @@
+
+
+
 var operacion = (operacion)=>{
-
-   $.ajax({
-    url:'../Controllers/operaciones.controller.php?operacion='
-   });
-
+    var numero1 = document.getElementById('numero1').value;
+    var numero2 = document.getElementById('numero2').value;
+    var url =`../Controllers/operaciones.controller.php?operacion=${operacion}&numero1=${numero1}&numero2=${numero2}`;
+    var url2="../Controllers/operaciones.controller.php?operacion="+operacion+"&numero1="+numero1+"&numero2="+numero2;
+    $.get(url,(resultado)=>{
+        console.log(resultado);
+        resultado = JSON.parse(resultado);
+        document.getElementById('res').value = resultado;
+    })
 }
+
