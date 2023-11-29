4f3dbc074aa3d37d6b6e67830776a41683838e86
Lleroc <llleroc@yahoo.es>
2023-11-29 18:48:30 -0500

    login_php

--- Proyecto4/login.php
+++ Proyecto4/login.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Modernize Free</title>
+  <title>Sitio Usuarios</title>
   <link rel="shortcut icon" type="image/png" href="Public/assets/images/logos/favicon.png" />
   <link rel="stylesheet" href="Public/assets/css/styles.min.css" />
 </head>
@@ -21,19 +21,19 @@
             <div class="card mb-0">
               <div class="card-body">
                 <a href="./index.html" class="text-nowrap logo-img text-center d-block py-3 w-100">
-                  <img src="Public/assets/images/logos/dark-logo.svg" width="180" alt="">
+                 <!-- <img src="Public/assets/images/logos/dark-logo.svg" width="180" alt="">-->
                 </a>
-                <p class="text-center">Your Social Campaigns</p>
+                <p class="text-center">Ingrese sus datos</p>
                 <form>
                   <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Username</label>
+                    <label for="exampleInputEmail1" class="form-label">Correo</label>
                     <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
                   </div>
                   <div class="mb-4">
-                    <label for="exampleInputPassword1" class="form-label">Password</label>
+                    <label for="exampleInputPassword1" class="form-label">Contraseña</label>
                     <input type="password" class="form-control" id="exampleInputPassword1">
                   </div>
-                  <div class="d-flex align-items-center justify-content-between mb-4">
+                <!--  <div class="d-flex align-items-center justify-content-between mb-4">
                     <div class="form-check">
                       <input class="form-check-input primary" type="checkbox" value="" id="flexCheckChecked" checked>
                       <label class="form-check-label text-dark" for="flexCheckChecked">
@@ -41,12 +41,12 @@
                       </label>
                     </div>
                     <a class="text-primary fw-bold" href="./index.html">Forgot Password ?</a>
-                  </div>
+                  </div>-->
                   <a href="./index.html" class="btn btn-primary w-100 py-8 fs-4 mb-4 rounded-2">Sign In</a>
-                  <div class="d-flex align-items-center justify-content-center">
+                 <!-- <div class="d-flex align-items-center justify-content-center">
                     <p class="fs-4 mb-0 fw-bold">New to Modernize?</p>
                     <a class="text-primary fw-bold ms-2" href="./authentication-register.html">Create an account</a>
-                  </div>
+                  </div>-->
                 </form>
               </div>
             </div>
