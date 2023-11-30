4feb8f2e7555101da22882f2d1a6cf12c8d398b9
Lleroc <llleroc@yahoo.es>
2023-11-29 19:09:53 -0500

    cls_conexion

---
+++ Proyecto4/Models/cls_conexion.model.php
@@ -0,0 +1,28 @@
+<?php
+class Clase_Conectar_Base_Datos
+{
+    public $conexion;
+    protected $db;
+    private $host = "localhost";  //192.168.100.103
+    private $user = "root";
+    private $pass = "root"; //esto es solo para MAMP  
+    /**
+     * XAMPP  password = '';
+     */
+    private $dbname = "Sexto";
+
+    public function ProcedimientoConectar()
+    {
+        $this->conexion = mysqli_connect($this->host, $this->user, $this->pass, $this->dbname);
+        mysqli_query($this->conexion, "SET NAMES 'utf8'");
+
+        if (!$this->conexion) {
+            die("Error en la conexion con el servidor" . mysqli_error($this->conexion));
+        }
+        //if(!$this->conexion) die("Error en la conexion con el servidor" . mysqli_error($this->conexion));
+
+        $this->db = mysqli_select_db($this->conexion, $this->dbname);
+
+        if (!$this->db) die("Error en la seleccion de la base de datos" . mysqli_error($this->conexion));
+    }
+}
