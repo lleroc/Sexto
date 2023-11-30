c9c41f712aa1dd6a9b35ec21fef8c78f4d607d34
Lleroc <llleroc@yahoo.es>
2023-11-29 20:37:46 -0500

    usuarios_validacion_sql_inyection

--- Proyecto4/Controllers/usuario.controller.php
+++ Proyecto4/Controllers/usuario.controller.php
@@ -5,31 +5,35 @@ switch ($_GET["op"]){
     case 'login':
         $correo = $_POST["correo"];
         $contrasenia = $_POST["contrasenia"];
-
         if(empty($correo) || empty($contrasenia)){
             header("Location:../login.php?op=1"); //llenar datos vacios
             exit();
         }
-
         try {
-            $datos = array();
-            $datos = $usuarios->login($correo, $contrasenia);
-            $respuesta = mysqli_fetch_assoc($datos);
-       
-            if(empty($respuesta)){
+            $datos = array(); //defino un arreglo
+            $datos = $usuarios->login($correo, $contrasenia); // almano en el arreglo la información de la base de datos
+            $respuesta = mysqli_fetch_assoc($datos); // declaro una variable "respuesta" para usar los valores que trae
+            if(is_array($respuesta) and count($respuesta)>0){  // comparar si la variable "respuesta" tiene datos y es un arreglo
+                //poner variables de session controlar accessos
+                //$respuesta -> trae toda la información del usuario
+                if($contrasenia == $respuesta["Contrasenia"]){  //comparar la contraseña de la base con la contraseña que ingreso el usuario
+                    $_SESSION['Nombres']  = $respuesta["Nombres"];
+                    $_SESSION['Apellidos'] = $respuesta["Apellidos"];
+                    header("Location:../views/index.php");
+                }else{
+                    header("Location:../login.php?op=2"); //el usuario o la contraseña son incorrectos
+                    exit();
+                }
+            }else{
                 header("Location:../login.php?op=2"); //el usuario o la contraseña son incorrectos
                 exit();
-            }
-            if(is_array($respuesta) and count($respuesta)>0){
-                //poner variables de session controlar accessos
-                header("Location:../views/index.php");
             }
         } catch (\Throwable $th) {
             echo json_encode($th->getMessage());
             header("Location:../login.php?op=3"); // no se que error escribir  es para capturar un error de codigo
         }
         break;
-   
+
 }
 
 
--- Proyecto4/Models/cls_usuario.model.php
+++ Proyecto4/Models/cls_usuario.model.php
@@ -2,13 +2,33 @@
 require_once('cls_conexion.model.php');
 class Clase_Usuarios
 {
+    public function todos()
+    {
+    }
+    public function uno($UsuarioId)
+    {
+    }
+    public function insertar($Cedula, $Nombres, $Apellidos, $Telefono, $Correo, $Contrasenia, $Rol)
+    {
+    }
+    public function actualizar($UsuarioId, $Cedula, $Nombres, $Apellidos, $Telefono, $Correo, $Contrasenia, $Rol)
+    {
+    }
+    public function eliminar($UsuarioId)
+    {
+    }
+    public function actualizar_contrasenia($UsuarioId, $contrasenia)
+    {
+    }
+
+
     public function login($correo, $contrasenia)
     {
         try {
             $con = new Clase_Conectar_Base_Datos();
             $con = $con->ProcedimientoConectar();
-            $cadena = "SELECT * FROM `Usuarios` WHERE `Correo`= '$correo' and `Contrasenia`='$contrasenia'";
-
+            //$cadena = sprintf("SELECT * FROM `Usuarios` WHERE `Correo`= '%s' and `Contrasenia`='%s'",mysqli_real_escape_string($con,$correo),mysqli_real_escape_string($con,$contrasenia));
+            $cadena = "SELECT * FROM `Usuarios` WHERE `Correo`= '$correo'";
             $result = mysqli_query($con, $cadena);
             return $result;
         } catch (Throwable $th) {
