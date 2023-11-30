6198883870a5c3236c5b268df1184a0f7f6cd4b3
Lleroc <llleroc@yahoo.es>
2023-11-29 19:44:54 -0500

    header_nombres_usuario

--- Proyecto4/Views/html/header.php
+++ Proyecto4/Views/html/header.php
@@ -15,7 +15,7 @@
           </ul>
           <div class="navbar-collapse justify-content-end px-0" id="navbarNav">
             <ul class="navbar-nav flex-row ms-auto align-items-center justify-content-end">
-             
+            <a  class="btn btn-primary"><?php echo $_SESSION['Nombres'] . " " .$_SESSION['Apellidos'];    ?></a>
               <li class="nav-item dropdown">
             
                 <a class="nav-link nav-icon-hover" href="javascript:void(0)" id="drop2" data-bs-toggle="dropdown"
