13e3ef3d7dfab0fd7575c71a230770cd5ddd290b
Lleroc <llleroc@yahoo.es>
2023-12-06 19:38:37 -0500

    Pais_Provincias

--- Proyecto4/Docs/Base_Datos.sql
+++ Proyecto4/Docs/Base_Datos.sql
@@ -1,3 +1,37 @@
+
+--
+-- Base de datos: `Sexto`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Pais`
+--
+
+CREATE TABLE `Pais` (
+  `PaisId` int(11) NOT NULL,
+  `Nombre` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Provincias`
+--
+
+CREATE TABLE `Provincias` (
+  `ProvinciasId` int(11) NOT NULL,
+  `Nombre` text NOT NULL,
+  `PaisesId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Usuarios`
+--
+
 CREATE TABLE `Usuarios` (
   `UsuarioId` int(11) NOT NULL,
   `Cedula` varchar(17) NOT NULL,
@@ -10,8 +44,30 @@ CREATE TABLE `Usuarios` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
+-- Volcado de datos para la tabla `Usuarios`
+--
+
+INSERT INTO `Usuarios` (`UsuarioId`, `Cedula`, `Nombres`, `Apellidos`, `Telefono`, `Correo`, `Contrasenia`, `Rol`) VALUES
+(1, '1803971371', 'Luis Antonio', 'Llerena Ocaña', '0987654321', 'lleroc1@gmail.com', '123', 'Administrador'),
+(2, '1234567890', 'Otro Luis', 'Otro Llerena', '0987654321', 'correo@gmail.com', '123', 'Vendedor'),
+(4, '1803971330', 'Luis Antonio', 'Llerena Ocaña', '0981030167', 'lleroc@gmail.com', '123', 'Administrador');
+
+--
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `Pais`
+--
+ALTER TABLE `Pais`
+  ADD PRIMARY KEY (`PaisId`);
+
+--
+-- Indices de la tabla `Provincias`
+--
+ALTER TABLE `Provincias`
+  ADD PRIMARY KEY (`ProvinciasId`),
+  ADD KEY `Pais_Provincia` (`PaisesId`);
 
 --
 -- Indices de la tabla `Usuarios`
@@ -24,8 +80,30 @@ ALTER TABLE `Usuarios`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `Pais`
+--
+ALTER TABLE `Pais`
+  MODIFY `PaisId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `Provincias`
+--
+ALTER TABLE `Provincias`
+  MODIFY `ProvinciasId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `Usuarios`
 --
 ALTER TABLE `Usuarios`
-  MODIFY `UsuarioId` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `UsuarioId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `Provincias`
+--
+ALTER TABLE `Provincias`
+  ADD CONSTRAINT `Pais_Provincia` FOREIGN KEY (`PaisesId`) REFERENCES `Pais` (`PaisId`);
 COMMIT;
