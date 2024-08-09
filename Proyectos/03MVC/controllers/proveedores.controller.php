82b2bdd97147aee3a802c7bd675a013e67e8dda7
lleroc <llleroc@yahoo.es>
2024-08-08 19:44:15 -0500

    angular

--- Proyectos/03MVC/controllers/proveedores.controller.php
+++ Proyectos/03MVC/controllers/proveedores.controller.php
@@ -1,5 +1,12 @@
 <?php
-
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+$method = $_SERVER["REQUEST_METHOD"];
+if ($method == "OPTIONS") {
+    die();
+}
 //TODO: controlador de proveedores
 
 require_once('../models/proveedores.model.php');
