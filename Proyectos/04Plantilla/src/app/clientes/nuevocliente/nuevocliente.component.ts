1985bcbabc299d66af5e14dfb7b7120c91575212
lleroc <llleroc@yahoo.es>
2024-08-22 20:26:48 -0500

    validadores

--- Proyectos/04Plantilla/src/app/clientes/nuevocliente/nuevocliente.component.ts
+++ Proyectos/04Plantilla/src/app/clientes/nuevocliente/nuevocliente.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
-
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ClientesService } from 'src/app/Services/clientes.service';
+import { ICliente } from 'src/app/Interfaces/icliente';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-nuevocliente',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './nuevocliente.component.html',
   styleUrl: './nuevocliente.component.scss'
 })
 export class NuevoclienteComponent {
+  frm_Cliente = new FormGroup({
+    //idClientes: new FormControl(),
+    Nombres: new FormControl('', Validators.required),
+    Direccion: new FormControl('', Validators.required),
+    Telefono: new FormControl('', Validators.required),
+    Cedula: new FormControl('', [Validators.required, Validators.minLength(10)]),
+    Correo: new FormControl('', [Validators.required, Validators.email])
+  });
+  idClientes = 0;
+  titulo = 'Nuevo Cliente';
+  constructor(private clienteServicio: ClientesService) {}
 
+  grabar() {
+    let cliente: ICliente = {
+      idClientes: this.idClientes,
+      Nombres: this.frm_Cliente.controls['Nombres'].value,
+      Direccion: this.frm_Cliente.controls['Direccion'].value,
+      Telefono: this.frm_Cliente.controls['Telefono'].value,
+      Cedula: this.frm_Cliente.controls['Cedula'].value,
+      Correo: this.frm_Cliente.controls['Correo'].value
+    };
+    console.log(cliente);
+  }
 }
