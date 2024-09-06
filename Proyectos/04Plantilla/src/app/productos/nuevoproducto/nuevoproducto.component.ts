6e9d7a0b2647424805dd15ba9396e48dd7819e4b
lleroc <llleroc@yahoo.es>
2024-09-05 20:42:46 -0500

    nuevoproductos

--- Proyectos/04Plantilla/src/app/productos/nuevoproducto/nuevoproducto.component.ts
+++ Proyectos/04Plantilla/src/app/productos/nuevoproducto/nuevoproducto.component.ts
@@ -1,15 +1,59 @@
-import { Component } from '@angular/core';
-import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Iproveedor } from 'src/app/Interfaces/iproveedor';
+import { IUnidadMedida } from 'src/app/Interfaces/iunidadmedida';
+import { ProveedorService } from 'src/app/Services/proveedores.service';
+import { UnidadmedidaService } from 'src/app/Services/unidadmedida.service';
 
 @Component({
   selector: 'app-nuevoproducto',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, FormsModule, CommonModule],
   templateUrl: './nuevoproducto.component.html',
   styleUrl: './nuevoproducto.component.scss'
 })
-export class NuevoproductoComponent {
+export class NuevoproductoComponent implements OnInit {
+  listaUnidadMedida: IUnidadMedida[] = [];
+  listaProveedores: Iproveedor[] = [];
   titulo = '';
   frm_Producto: FormGroup;
+  constructor(
+    private uniadaServicio: UnidadmedidaService,
+    private fb: FormBuilder,
+    private proveedoreServicio: ProveedorService
+  ) {}
+  ngOnInit(): void {
+    this.uniadaServicio.todos().subscribe((data) => (this.listaUnidadMedida = data));
+    this.proveedoreServicio.todos().subscribe((data) => (this.listaProveedores = data));
+
+    this.crearFormulario();
+  }
+
+  crearFormulario() {
+    /* this.frm_Producto = this.fb.group({
+      Codigo_Barras: ['', Validators.required],
+      Nombre_Producto: ['', Validators.required],
+      Graba_IVA: ['', Validators.required],
+      Unidad_Medida_idUnidad_Medida: ['', Validators.required],
+      IVA_idIVA: ['', Validators.required],
+      Cantidad: ['', [Validators.required, Validators.min(1)]],
+      Valor_Compra: ['', [Validators.required, Validators.min(0)]],
+      Valor_Venta: ['', [Validators.required, Validators.min(0)]],
+      Proveedores_idProveedores: ['', Validators.required]
+    });*/
+    this.frm_Producto = new FormGroup({
+      Codigo_Barras: new FormControl('', Validators.required),
+      Nombre_Producto: new FormControl('', Validators.required),
+      Graba_IVA: new FormControl('', Validators.required),
+      Unidad_Medida_idUnidad_Medida: new FormControl('', Validators.required),
+      IVA_idIVA: new FormControl('', Validators.required),
+      Cantidad: new FormControl('', [Validators.required, Validators.min(1)]),
+      Valor_Compra: new FormControl('', [Validators.required, Validators.min(0)]),
+      Valor_Venta: new FormControl('', [Validators.required, Validators.min(0)]),
+      Proveedores_idProveedores: new FormControl('', Validators.required)
+    });
+  }
+
   grabar() {}
 }
